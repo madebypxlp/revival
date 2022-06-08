a6eb57fe5322de53ed0b4e173da7b5a31a024f69
fabiangigler <fabian@byway.at>
2022-06-08 19:56:36 +0200

    minor fixes

--- components/modules/ImageWithInfos/ImageWithInfos.tsx
+++ components/modules/ImageWithInfos/ImageWithInfos.tsx
@@ -4,6 +4,7 @@ import IImageWithInfos from './ImageWithInfos.interface'
 import UIImage from '@components/ui/Image/Image'
 import Image from 'next/image'
 import Button from '@components/ui/Button/Button'
+import cn from 'classnames'
 
 const ImageWithInfosModule: FunctionComponent<{ module: IImageWithInfos }> = ({
   module,
@@ -13,24 +14,37 @@ const ImageWithInfosModule: FunctionComponent<{ module: IImageWithInfos }> = ({
   return (
     <div className={`${styles.root} relative`}>
       <UIImage image={image} layout="fill" className="absolute inset-0" />
-      <div className="min-h-[55.5rem] flex justify-center items-center container relative z-10">
+      <div className="container flex justify-center items-center min-h-[55.5rem] relative z-10">
         <div className="flex items-center justify-center flex-col">
-          <div className="flex flex-row items-center mb-10">
-            {subline && <div className="typo-h6 text-white">{subline}</div>}
+          <div
+            className={cn([
+              'flex flex-row items-center',
+              !!facts?.length ? 'mb-50' : 'mb-10',
+            ])}
+          >
+            {subline && (
+              <div
+                className={cn([
+                  'text-white',
+                  !!facts?.length ? 'typo-h6' : 'typo-eyebrow',
+                ])}
+              >
+                {subline}
+              </div>
+            )}
             {sublineImage?.sourceUrl && (
-              <div className="relative w-25 h-30 ml-5 md:ml-15">
+              <div className="relative ml-5 md:ml-15">
                 <Image
                   src={sublineImage.sourceUrl}
                   alt={sublineImage.altText}
-                  layout="fill"
+                  width={25}
+                  height={30}
                 />
               </div>
             )}
           </div>
 
-          {headline && (
-            <h3 className="typo-h3 text-white mb-20 md:mb-0">{headline}</h3>
-          )}
+          {headline && <h3 className="typo-h3 text-white mb-20">{headline}</h3>}
 
           {copy && (
             <p className="typo-fact text-white w-[45rem] max-w-full text-center mb-10">
@@ -39,18 +53,19 @@ const ImageWithInfosModule: FunctionComponent<{ module: IImageWithInfos }> = ({
           )}
 
           {!!facts?.length && (
-            <div className="my-40 hidden lg:flex">
+            <div className="mt-10 mb-30 hidden lg:flex">
               {facts.map((fact) => {
                 return (
                   <div
                     className={`${styles.fact} flex items-center md:mx-7 typo-fact text-white`}
                   >
                     {fact?.icon?.sourceUrl && (
-                      <div className="relative w-[22px] h-[22px] mr-15">
+                      <div className="relative mr-15">
                         <Image
                           src={fact.icon.sourceUrl}
                           alt={fact.icon.altText}
-                          layout="fill"
+                          width={22}
+                          height={22}
                         />
                       </div>
                     )}
