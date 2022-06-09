e86d352567431e3e75249a2fc87d22a502d2fb8c
KyleMitton711 <upworkvancouver@gmail.com>
2022-06-09 10:51:55 -0700

    update our brand

--- components/modules/OurBrands/OurBrands.tsx
+++ components/modules/OurBrands/OurBrands.tsx
@@ -2,26 +2,35 @@ import React, { FunctionComponent } from 'react'
 import Image from 'next/image'
 import styles from './OurBrands.module.scss'
 import IOurBrands from './OurBrands.interface'
+import ArrowCTA from '@components/ui/ArrowCTA/ArrowCTA'
 
 const OurBrandsModule: FunctionComponent<{ module: IOurBrands }> = ({
   module,
 }) => {
-  console.log(module)
   return (
-    <div className={`${styles.root} container pt-100`}>
+    <div className={`${styles.root} container pt-40 md:pt-100`}>
       {module.rows.map((row, idx) => (
-        <div key={idx} className={`default-grid mb-90`}>
-          <div className="col-span-12">
+        <div key={idx} className={`default-grid pb-35 md:pb-90`}>
+          <div className="col-span-2 md:col-span-12 flex flex-col md:flex-row md:justify-between items-start md:items-end mb-15 md:mb-35 pr-40 md:pr-10">
             {row.headline && (
-              <h2 className={styles.headline}>{row.headline}</h2>
+              <span className={styles.headline}>{row.headline}</span>
+            )}
+            {row.brands.length > 3 && (
+              <div className="relative mt-20 md:mt-0">
+                <ArrowCTA color="blue" orientation="right" href="">
+                  Shop All
+                </ArrowCTA>
+              </div>
             )}
           </div>
           {row.brands.map((brand, brandIdx) => (
             <div
               key={brandIdx}
-              className={
-                row.brands.length > 3 ? `col-span-3 my-10` : 'col-span-4 my-10'
-              }
+              className={`${
+                row.brands.length > 3
+                  ? 'md:col-span-3 col-span-1'
+                  : 'md:col-span-4 col-span-2'
+              } my-5 md:my-10`}
             >
               <a href={brand.url} target="_blank" className={styles.brandLink}>
                 <Image
