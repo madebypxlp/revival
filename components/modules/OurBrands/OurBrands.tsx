7ba7b6304c2cbc12217d07bbf4f2996d2204d341
KyleMitton711 <upworkvancouver@gmail.com>
2022-06-09 03:16:22 -0700

    update our brand module

--- components/modules/OurBrands/OurBrands.tsx
+++ components/modules/OurBrands/OurBrands.tsx
@@ -1,14 +1,41 @@
 import React, { FunctionComponent } from 'react'
+import Image from 'next/image'
 import styles from './OurBrands.module.scss'
 import IOurBrands from './OurBrands.interface'
 
-const OurBrandsModule:FunctionComponent<{ module: IOurBrands }> = ({ module }) => {
+const OurBrandsModule: FunctionComponent<{ module: IOurBrands }> = ({
+  module,
+}) => {
   console.log(module)
   return (
-    <div
-      className={`${styles.root} container`}
-    >
-      Ourbrands Module
+    <div className={`${styles.root} container pt-100`}>
+      {module.rows.map((row, idx) => (
+        <div key={idx} className={`default-grid mb-90`}>
+          <div className="col-span-12">
+            {row.headline && (
+              <h2 className={styles.headline}>{row.headline}</h2>
+            )}
+          </div>
+          {row.brands.map((brand, brandIdx) => (
+            <div
+              key={brandIdx}
+              className={
+                row.brands.length > 3 ? `col-span-3 my-10` : 'col-span-4 my-10'
+              }
+            >
+              <a href={brand.url} target="_blank" className={styles.brandLink}>
+                <Image
+                  src={brand.brandLogo.sourceUrl}
+                  alt={brand.brandLogo.altText}
+                  layout={'responsive'}
+                  width={452}
+                  height={230}
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
