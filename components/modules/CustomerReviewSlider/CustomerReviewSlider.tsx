e6a67d23669612e7da74f7bf17f95aef76ca78f4
Federico Rivero <frivero@fing.edu.uy>
2022-06-15 19:57:09 -0300

    Customer review slider trying out HeroCarousel

--- components/modules/CustomerReviewSlider/CustomerReviewSlider.tsx
+++ components/modules/CustomerReviewSlider/CustomerReviewSlider.tsx
@@ -1,17 +1,15 @@
 import React, { FunctionComponent } from 'react'
 import styles from './CustomerReviewSlider.module.scss'
 import ICustomerReviewSlider from './CustomerReviewSlider.interface'
+import HeroCarousel from '../HeroCarousel/HeroCarousel'
 
-const CustomerReviewSliderModule:FunctionComponent<{ module: ICustomerReviewSlider }> = ({ module }) => {
+const CustomerReviewSliderModule: FunctionComponent<{
+  module: ICustomerReviewSlider
+}> = ({ module }) => {
   console.log(module)
-  return (
-    <div
-      className={`${styles.root} container`}
-    >
-      Customerreviewslider Module
-      {console.log(module)}
-    </div>
-  )
+  const { headline, reviews } = module
+
+  return <div className={`${styles.root}`}></div>
 }
 
 export default CustomerReviewSliderModule
