dfda437ef6499283e9176ec33f968e293cb18a65
Federico Rivero <frivero@fing.edu.uy>
2022-06-15 11:50:35 -0300

    Light hero component

--- components/modules/LightHero/LightHero.tsx
+++ components/modules/LightHero/LightHero.tsx
@@ -1,12 +1,18 @@
 import React, { FunctionComponent } from 'react'
+import c from 'classnames'
 import styles from './LightHero.module.scss'
 import ILightHero from './LightHero.interface'
 
 const LightHeroModule: FunctionComponent<{ module: ILightHero }> = ({
   module,
 }) => {
-  console.log(module)
-  return <div className={`${styles.root} container`}>Lighthero Module</div>
+  const { headline, subline } = module
+  return (
+    <div className={`${styles.root} container`}>
+      <div className={c(styles.headline)}>{headline}</div>
+      <div className={c(styles.subline)}>{subline}</div>
+    </div>
+  )
 }
 
 export default LightHeroModule
