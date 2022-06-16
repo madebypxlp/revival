18ca3739fbf58be890f9fc1d5adb5cd04d24947c
Federico Rivero <frivero@fing.edu.uy>
2022-06-16 11:17:17 -0300

    Adding swiper

--- components/modules/CustomerReviewSlider/CustomerReviewSlider.tsx
+++ components/modules/CustomerReviewSlider/CustomerReviewSlider.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent } from 'react'
+import c from 'classnames'
 import styles from './CustomerReviewSlider.module.scss'
 import ICustomerReviewSlider from './CustomerReviewSlider.interface'
-import HeroCarousel from '../HeroCarousel/HeroCarousel'
+import { Swiper, SwiperSlide } from '@components/ui/Swiper/Swiper'
 
 const CustomerReviewSliderModule: FunctionComponent<{
   module: ICustomerReviewSlider
@@ -9,7 +10,31 @@ const CustomerReviewSliderModule: FunctionComponent<{
   console.log(module)
   const { headline, reviews } = module
 
-  return <div className={`${styles.root}`}></div>
+  return (
+    <div className={`${styles.root}`}>
+      <div className={c(styles.headlineContainer)}>
+        {headline && <span>{headline}</span>}
+      </div>
+      {reviews && reviews.length > 0 && (
+        <Swiper
+          centeredSlides
+          centerInsufficientSlides
+          slidesPerView={2}
+          navigation
+          loop
+        >
+          {reviews.map((r) => (
+            <SwiperSlide>
+              <div className={c(styles.reviewCard)}>
+                <div>{r.reviewCopy}</div>
+                <div>{r.author}</div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </div>
+  )
 }
 
 export default CustomerReviewSliderModule
