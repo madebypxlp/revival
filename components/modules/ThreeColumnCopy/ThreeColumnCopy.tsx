e3fe9e8d774de05c2ab1f06c0de459c20d222bb9
Federico Rivero <frivero@fing.edu.uy>
2022-06-16 19:41:13 -0300

    Three column, WYSIWYG column

--- components/modules/ThreeColumnCopy/ThreeColumnCopy.tsx
+++ components/modules/ThreeColumnCopy/ThreeColumnCopy.tsx
@@ -1,14 +1,26 @@
 import React, { FunctionComponent } from 'react'
 import styles from './ThreeColumnCopy.module.scss'
+import c from 'classnames'
+import parse from 'html-react-parser'
 import IThreeColumnCopy from './ThreeColumnCopy.interface'
 
-const ThreeColumnCopyModule:FunctionComponent<{ module: IThreeColumnCopy }> = ({ module }) => {
+const ThreeColumnCopyModule: FunctionComponent<{
+  module: IThreeColumnCopy
+}> = ({ module }) => {
   console.log(module)
+  const { subline, headline, columns, backgroundColor } = module
+
   return (
     <div
-      className={`${styles.root} container`}
+      className={`${styles.root} container default-grid`}
+      style={{ backgroundColor }}
     >
-      Threecolumncopy Module
+      {subline && <div className={styles.subline}>{subline}</div>}
+      {headline && <h3 className={styles.headline}>{headline}</h3>}
+      {columns &&
+        columns.map((col, index) => {
+          return <div className={styles.contentColumn}>{parse(col.copy)}</div>
+        })}
     </div>
   )
 }
