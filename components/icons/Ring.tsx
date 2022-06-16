9916b9a883a87fb8735cfc931bc8df31c3bbe5c3
Federico Rivero <frivero@fing.edu.uy>
2022-06-16 01:43:52 -0300

    CarouselHero texts and rings

---
+++ components/icons/Ring.tsx
@@ -0,0 +1,22 @@
+const Ring = ({ ...props }) => {
+  return (
+    <svg
+      width="334"
+      height="333"
+      viewBox="0 0 334 333"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle
+        cx="167.498"
+        cy="166.5"
+        r="156.5"
+        stroke="#FFF5D1"
+        stroke-width="20"
+      />
+    </svg>
+  )
+}
+
+export default Ring
--- components/modules/CarouselHero/CarouselHero.tsx
+++ components/modules/CarouselHero/CarouselHero.tsx
@@ -1,14 +1,36 @@
 import React, { FunctionComponent } from 'react'
+import c from 'classnames'
 import styles from './CarouselHero.module.scss'
 import ICarouselHero from './CarouselHero.interface'
+import Button from '@components/ui/Button/Button'
+import Ring from '@components/icons/Ring'
 
-const CarouselHeroModule:FunctionComponent<{ module: ICarouselHero }> = ({ module }) => {
+const CarouselHeroModule: FunctionComponent<{ module: ICarouselHero }> = ({
+  module,
+}) => {
   console.log(module)
+  const { headline, copy, images, link, subline } = module
   return (
-    <div
-      className={`${styles.root} container`}
-    >
-      Carouselhero Module
+    <div className={`${styles.root}`}>
+      <div className={'container default-grid'}>
+        <div className={c(styles.subline)}>{subline}</div>
+        <h2 className={c(styles.headline)}>{headline}</h2>
+        <div className={c(styles.copy)}>{copy}</div>
+        <div className={c(styles.buttonContainer)}>
+          <Button
+            link={link}
+            className={c(styles.button)}
+            type="default"
+            variant="large"
+            color="yellow"
+          />
+        </div>
+      </div>
+      <div
+        className={'absolute top-0 left-0 w-full h-full container default-grid'}
+      >
+        <Ring className={'mt-255 col-start-6 col-span-3'} />
+      </div>
     </div>
   )
 }
