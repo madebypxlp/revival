d13a405d56b35e93d32844316beb837cb586fc6e
Federico Rivero <frivero@fing.edu.uy>
2022-06-17 12:49:28 -0300

    Resource Grid - Headline and layout

--- components/modules/ResourceGrid/ResourceGrid.tsx
+++ components/modules/ResourceGrid/ResourceGrid.tsx
@@ -2,13 +2,17 @@ import React, { FunctionComponent } from 'react'
 import styles from './ResourceGrid.module.scss'
 import IResourceGrid from './ResourceGrid.interface'
 
-const ResourceGridModule:FunctionComponent<{ module: IResourceGrid }> = ({ module }) => {
+const ResourceGridModule: FunctionComponent<{ module: IResourceGrid }> = ({
+  module,
+}) => {
   console.log(module)
+
+  const { headline, link, featuredResource } = module
   return (
-    <div
-      className={`${styles.root} container`}
-    >
-      Resourcegrid Module
+    <div className={`${styles.root} container default-grid`}>
+      <div className={styles.headlineContainer}>
+        {headline && <h4>{headline}</h4>}
+      </div>
     </div>
   )
 }
