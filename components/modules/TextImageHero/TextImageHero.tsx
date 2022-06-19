c0723ecae8ecc3ef3a7969ed1a6a694f4dc72026
Alexander Schwaiger <alex.schwaiger@pxlp.dev>
2022-06-19 09:18:51 +0200

    removed text

--- components/modules/TextImageHero/TextImageHero.tsx
+++ components/modules/TextImageHero/TextImageHero.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent, useEffect, useRef } from 'react'
-import styles from './TextImageHero.module.scss'
 import ITextImageHero from './TextImageHero.interface'
 import Text from '@components/ui/Text/Text'
 import ImageComponent from '@components/ui/Image/Image'
+import parse from 'html-react-parser'
+import styles from './TextImageHero.module.scss'
 
 const TextImageHeroModule: FunctionComponent<{ module: ITextImageHero }> = ({
   module,
@@ -15,7 +16,7 @@ const TextImageHeroModule: FunctionComponent<{ module: ITextImageHero }> = ({
     const layoutWidth = (document as any).querySelector('main.fit').clientWidth
     const containerWidth = (gridRef.current as any).clientWidth
     let width = '100%'
-    if (window.innerWidth >= 768) {
+    if (window.innerWidth >= 1024) {
       width = `calc(100% + ${(layoutWidth - containerWidth) / 2}px)`
     }
     ;(imageRef.current as any).style.width = width
@@ -28,16 +29,16 @@ const TextImageHeroModule: FunctionComponent<{ module: ITextImageHero }> = ({
     return () => {
       window.removeEventListener('resize', setWidth)
     }
-  }, [process.browser, imageRef.current, gridRef.current])
+  }, [imageRef.current, gridRef.current])
   return (
     <div className={`${styles.root} container`}>
-      <div className={`default-grid`} ref={gridRef}>
-        <div className={`col-span-2 md:col-span-5 ${styles.text_block}`}>
-          <Text className={styles.subline}>{subline}</Text>
-          <Text className={styles.headline}>{headline}</Text>
-          <Text className={styles.copy}>{copy}</Text>
+      <div className={`default-grid-lg`} ref={gridRef}>
+        <div className={`col-span-2 lg:col-span-5 ${styles.text_block}`}>
+          <h3 className={styles.subline}>{subline}</h3>
+          <h2 className={styles.headline}>{headline}</h2>
+          <div className={styles.copy}>{parse(copy)}</div>
         </div>
-        <div className="col-span-2 md:col-span-7">
+        <div className="col-span-2 lg:col-span-7">
           <div className="image-wrapper" ref={imageRef}>
             <ImageComponent image={image} className={styles.image} />
           </div>
