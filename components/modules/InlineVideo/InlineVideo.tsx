14271b5b61c91ecbb56932a35236cdf272669ee2
Florian Rasser <florian@byway.at>
2022-06-20 14:30:10 +0200

    grid fix

--- components/modules/InlineVideo/InlineVideo.tsx
+++ components/modules/InlineVideo/InlineVideo.tsx
@@ -14,11 +14,11 @@ const InlineVideoModule: FunctionComponent<{ module: IInlineVideo }> = ({
       {youtubeId && (
         <Video
           source={youtubeId}
-          className="col-span-12 md:col-span-8 md:col-start-3 mb-45"
+          className="col-span-2 md:col-span-8 md:col-start-3 mb-45"
         />
       )}
       {title && (
-        <div className="col-span-12 md:col-span-8 md:col-start-3 typo-h4 text-blue-default">
+        <div className="col-span-2 md:col-span-8 md:col-start-3 typo-h4 text-blue-default">
           {title}
         </div>
       )}
