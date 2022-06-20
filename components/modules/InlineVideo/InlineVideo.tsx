e69e4a9c97a155e489550f1bb459e5b8ce0f602c
Florian Rasser <florian@byway.at>
2022-06-20 15:41:27 +0200

    add border radius

--- components/modules/InlineVideo/InlineVideo.tsx
+++ components/modules/InlineVideo/InlineVideo.tsx
@@ -12,10 +12,9 @@ const InlineVideoModule: FunctionComponent<{ module: IInlineVideo }> = ({
   return (
     <div className={`${styles.root} container default-grid mb-80`}>
       {youtubeId && (
-        <Video
-          source={youtubeId}
-          className="col-span-2 md:col-span-8 md:col-start-3 mb-45"
-        />
+        <div className="rounded-15 overflow-hidden col-span-2 md:col-span-8 md:col-start-3 mb-45 ">
+          <Video source={youtubeId} className="" />
+        </div>
       )}
       {title && (
         <div className="col-span-2 md:col-span-8 md:col-start-3 typo-h4 text-blue-default">
