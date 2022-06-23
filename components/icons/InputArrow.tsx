37c597c53cc18c482a4589ff74d42a20ac461fc8
Alexander Schwaiger <alex.schwaiger@pxlp.dev>
2022-06-23 08:50:48 +0200

    fixes

--- components/icons/InputArrow.tsx
+++ components/icons/InputArrow.tsx
@@ -10,15 +10,15 @@ const InputArrow = ({ ...props }) => {
       <path
         d="M8.77734 1L15.7773 8L8.77734 15"
         stroke="#00338D"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M0.999567 8H15.7773"
         stroke="#00338D"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
       />
     </svg>
   )
