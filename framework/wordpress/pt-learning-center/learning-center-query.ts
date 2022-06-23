97a5816c9bce3dfe264c9661d8edcff6a08a3039
Alexander Schwaiger <alex.schwaiger@pxlp.dev>
2022-06-23 09:07:49 +0200

    new module

--- framework/wordpress/pt-learning-center/learning-center-query.ts
+++ framework/wordpress/pt-learning-center/learning-center-query.ts
@@ -5,6 +5,7 @@ import InlineVideoFragment from '@components/modules/InlineVideo/InlineVideo.gra
 import WYSIWYGFragment from '@components/modules/WYSIWYG/WYSIWYG.graphql'
 import MoreArticlesFragment from '@components/modules/MoreArticles/MoreArticles.graphql'
 import NewsletterSignUpFragment from '@components/modules/NewsletterSignUp/NewsletterSignUp.graphql'
+import ProductSliderWithBigImageFragment from '@components/modules/ProductSliderWithBigImage/ProductSliderWithBigImage.graphql'
 
 const TEMPLATE = 'Learning_center_Detailpagelearningcenter_PageBuilder'
 export default `
@@ -15,6 +16,7 @@ export default `
   ${InlineImageFragment(TEMPLATE, true)}
   ${MoreArticlesFragment(TEMPLATE)}
   ${NewsletterSignUpFragment(TEMPLATE, true)}
+  ${ProductSliderWithBigImageFragment(TEMPLATE)}
   query learningCenter($slug: String) {
     additionalData: allLearningCenter(last: 5) {
       nodes {
@@ -56,6 +58,7 @@ export default `
           ...InlineImage_${TEMPLATE}
           ...MoreArticles_${TEMPLATE}
           ...NewsletterSignUp_${TEMPLATE}
+          ...ProductSliderWithBigImage_${TEMPLATE}
         }
         authorBioCopy
         authorName
