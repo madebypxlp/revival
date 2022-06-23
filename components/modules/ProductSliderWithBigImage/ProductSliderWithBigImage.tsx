f1df1708ee9294541f7180982ebf0c173a4cdc4d
Florian Rasser <florian@byway.at>
2022-06-23 14:48:04 +0200

    wip product slider with big image

--- components/modules/ProductSliderWithBigImage/ProductSliderWithBigImage.tsx
+++ components/modules/ProductSliderWithBigImage/ProductSliderWithBigImage.tsx
@@ -10,34 +10,15 @@ const ProductSliderWithBigImageModule: FunctionComponent<{
 
   const Slide = (
     <SwiperSlide>
-      <div className="h-500 border-2">
-        <div className="h-330 border-2"></div>
+      <div className="h-330 border-2">
+        <div className="h-250 border-2"></div>
       </div>
     </SwiperSlide>
   )
   return (
-    <div
-      className={`${styles.root} bg-cream pt-60 pb-80 mb-60 md:mb-80 overflow-hidden`}
-    >
-      <div className="container">
-        <h2 className="typo-h4 text-blue mb-35">Shop The Article</h2>
-        <Swiper
-          className="overflow-visible"
-          slidesPerView={1.2}
-          spaceBetween={20}
-          navigation
-          breakpoints={{
-            768: {
-              slidesPerView: 2.1,
-            },
-            1024: {
-              slidesPerView: 3.1,
-            },
-            1440: {
-              slidesPerView: 4.1,
-            },
-          }}
-        >
+    <div className={`${styles.root} container default-grid`}>
+      <div className="col-span-3 col-start-3 pr-65">
+        <Swiper slidesPerView={1} spaceBetween={20} navigation breakpoints={{}}>
           {Slide}
           {Slide}
           {Slide}
