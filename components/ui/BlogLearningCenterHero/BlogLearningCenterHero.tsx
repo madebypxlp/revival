f1f85f160aab83d309e4be57fbb2a7b226327398
Florian Rasser <florian@byway.at>
2022-06-30 12:05:46 +0200

    styling fixes

--- components/ui/BlogLearningCenterHero/BlogLearningCenterHero.tsx
+++ components/ui/BlogLearningCenterHero/BlogLearningCenterHero.tsx
@@ -10,6 +10,8 @@ import CopyIconRound from '@components/icons/CopyIconRound'
 import TwitterIconRound from '@components/icons/TwitterIconRound'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useIsMobile } from '@commerce/utils/hooks'
+import BlogHeroCircle from '@components/icons/BlogHeroCircle'
 
 const formatDate = (dateObject: string) => {
   const date = new Date(dateObject).toLocaleString('en-us', {
@@ -25,8 +27,9 @@ const BlogLearningCenterHero: FunctionComponent<
   LearningCenterInterface | PostInterface
 > = (props) => {
   const { categories, date, title, featuredImage, uri } = props
-
   let data
+
+  const isMobile = useIsMobile()
 
   if ('detailPageLearningCenter' in props) {
     data = props.detailPageLearningCenter
@@ -44,7 +47,10 @@ const BlogLearningCenterHero: FunctionComponent<
   const author = `By ${data?.authorName}`
 
   const icons = [
-    { el: <FacebookIconRound />, href: data?.socialLinks.facebook },
+    {
+      el: <FacebookIconRound />,
+      href: data?.socialLinks.facebook,
+    },
     { el: <TwitterIconRound />, href: data?.socialLinks.twitter },
     { el: <LinkedInIconRound />, href: data?.socialLinks.linkedin },
   ]
@@ -53,7 +59,9 @@ const BlogLearningCenterHero: FunctionComponent<
     if (!icon.href) return null
     return (
       <Link href={icon.href}>
-        <a className="inline-block w-75 h-75">{icon.el}</a>
+        <a className="inline-block w-[60px] h-[60px] md:w-[76px] md:h-[76px] flex-shrink-0">
+          {icon.el}
+        </a>
       </Link>
     )
   })
@@ -62,34 +70,49 @@ const BlogLearningCenterHero: FunctionComponent<
     navigator.clipboard.writeText(window.location.toString())
   }
 
-  return (
-    <div className={`${styles.root} default-grid gap-20 lg:gap-0 lg:pb-80`}>
-      <div className="container lg:col-start-1 col-span-full lg:col-span-6 lg:row-span-1 lg:row-start-3 lg:mb-40">
-        <h6 className="uppercase typo-category text-blue max-w-[57rem]">
-          {categoriesArray.join(', ')}
-        </h6>
-        <h2 className="text-blue ">{title}</h2>
-        <p className="text-20 leading-30 font-normal">{author}</p>
-        <p className="text-20 leading-30 font-normal">{formatDate(date)}</p>
-      </div>
-
-      <div className="-30 col-span-full aspect-w-[1280] aspect-h-[720] lg:col-span-6 relative lg:row-span-6">
+  const ImageComponent = (
+    <div className={styles.imageWrapper}>
+      <div className="aspect-w-[771] aspect-h-[500]">
         <Image
           src={featuredImage.node.sourceUrl}
           alt={featuredImage.node.altText}
           layout="fill"
-          className="z-20 rounded-bl"
+          objectFit="cover"
+          className="md:z-20 md:rounded-bl-[30px]"
         />
-        <div className={`${styles.circle} hidden lg:block`}></div>
+        <div className={`${styles.circle} hidden md:block`}>
+          <BlogHeroCircle />
+        </div>
       </div>
+    </div>
+  )
 
-      <div className="container flex justify-center items-center lg:block col-span-full lg:col-span-6 lg:row-span-1 lg:row-start-4">
+  return (
+    <div
+      className={`${styles.root} container default-grid gap-20 md:gap-0 md:pb-80`}
+    >
+      <div className="md:col-start-1 col-span-full pt-30 md:pt-0 md:col-span-6 md:row-span-1 md:row-start-3 md:mb-40">
+        <h6 className="uppercase typo-category text-blue max-w-[57rem]">
+          {categoriesArray.join(', ')}
+        </h6>
+        <h2 className="text-blue">{title}</h2>
+        <p className="text-20 leading-30 font-normal">{author}</p>
+        <p className="text-20 leading-30 font-normal mb-20 md:mb-0">
+          {formatDate(date)}
+        </p>
+        {isMobile && ImageComponent}
+      </div>
+      {!isMobile && ImageComponent}
+      <div className="flex justify-center items-center md:block col-span-full md:col-span-6 md:row-span-1 md:row-start-4 md:-mx-14 ">
         {links}
-        <button className="inline-block w-75 h-75" onClick={onClickCopy}>
+        <button
+          className="inline-block w-[60px] h-[60px] md:w-[76px] md:h-[76px] flex-shrink-0"
+          onClick={onClickCopy}
+        >
           <CopyIconRound />
         </button>
         <button
-          className="inline-block w-75 h-75"
+          className="inline-block w-[60px] h-[60px] md:w-[76px] md:h-[76px] flex-shrink-0 "
           onClick={() => window.print()}
         >
           <PrintIconRound />
